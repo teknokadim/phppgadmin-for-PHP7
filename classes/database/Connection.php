91657fe50b8cb242549b2b4a62b51f31355b824e
teknokadim <47940985+teknokadim@users.noreply.github.com>
2019-02-24 14:06:28 +0300

    Update Connection.php
    prosql 10 installed but return null value

--- classes/database/Connection.php
+++ classes/database/Connection.php
@@ -90,9 +90,7 @@ class Connection {
 
 		/* All <7.4 versions are not supported */
 		// if major version is 7 or less and wasn't cought in the
-		// switch/case block, we have an unsupported version.
-		if ((int)substr($version, 0, 1) < 8)
-			return null;
+		// switch/case block, we have an unsupported version
 
 		// If unknown version, then default to latest driver
 		return 'Postgres';
